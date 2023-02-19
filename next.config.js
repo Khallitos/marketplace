3e85fb8d0a25565557270f0a6854233933dc783e
khallitos <carlphil9924@gmail.com>
2023-02-19 14:13:45 +0000

    kk

--- next.config.js
+++ next.config.js
@@ -1,14 +1,2 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'kanmusic.s3.eu-west-2.amazonaws.com',
-        port: '',
-        
-      },
-    ],
-  },
-}
